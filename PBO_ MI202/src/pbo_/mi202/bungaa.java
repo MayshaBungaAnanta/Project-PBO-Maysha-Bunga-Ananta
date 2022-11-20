883ef2a17bee07098dea7d585252e388efe3373a
MayshaBungaAnanta <114631760+MayshaBungaAnanta@users.noreply.github.com>
2022-11-20 20:12:39 +0700

    PBO_2101092031
    Maysha Bunga Ananta
    2101092031
    MI 2B

---
+++ PBO_ MI202/src/pbo_/mi202/bungaa.java
@@ -0,0 +1,13 @@
+package pbo_.mi202;
+
+class Bunga {
+    public void Nama(){
+        System.out.println("Namaku bunga");
+    }
+    public void Umur(){
+        System.out.println("Umurku 20 tahun");
+    }
+}
+
+
+        
